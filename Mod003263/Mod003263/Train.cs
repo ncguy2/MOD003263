890f0d13fa927482986053c2eb7c237fd3d80dd1
Nick Guy <nick.guy@hotmail.co.uk>
2016-09-19 15:01:31 +0100

    Train form created

---
+++ Mod003263/Mod003263/Train.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mod003263 {
+    public partial class Train : Form {
+        public Train() {
+            InitializeComponent();
+        }
+    }
+}
