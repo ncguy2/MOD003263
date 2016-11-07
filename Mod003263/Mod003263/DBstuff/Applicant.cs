db1873617ae14c9a888b2dff409584fca5b19dc9
Callum highley <callum.highley@live.co.uk>
2016-11-07 16:09:06 +0000

    Start to Database Driver

---
+++ Mod003263/Mod003263/DBstuff/Applicant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod003263.DBstuff {
+    class Applicant {
+        private int m_id;
+        public int ID { get { return m_id; } set { m_id = value; } }
+
+        private String m_First_Name;
+        public String First_Name { get { return m_First_Name; } set { m_First_Name = value; } }
+
+        private String m_Last_Name;
+        public String Last_Name { get { return m_Last_Name; } set { m_Last_Name = value; } }
+
+        private String m_Applying_Position;
+        public String Applying_Position { get { return m_Applying_Position; } set { m_Applying_Position = value; } }
+
+        private String m_Picture;
+        public String Picture { get { return m_Picture; } set { m_Picture = value; } }
+
+        private String m_address;
+        public String Address { get { return m_address; } set { m_address = value; } }
+
+        private String m_Phone_Number;
+        public String Phone_Number { get { return m_Phone_Number; } set { m_Phone_Number = value; } }
+
+        private String m_email;
+        public String Email { get { return m_email; } set { m_email = value; } }
+
+        private long m_Dob;
+        public long Dob { get { return m_Dob; } set { m_Dob = value; } }
+    }
+}
---
+++ Mod003263/Mod003263/DBstuff/Database Connection.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Mod003263.DBstuff {
+    class Database_Connection {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public Database_Connection() {
+            Initialize();
+        }
+
+        private void Initialize() {
+            server = "HappyTec Server";
+            database = "Applicant";
+            uid = "admin";
+            password = "123";
+            string connectionString = "SERVER=" + server + "; DATABASE=" +
+                database + "; UserID=" + uid + "; PASSWORD=" + password;
+
+            connection = new MySqlConnection(connectionString);
+        }
+        //open connection to database
+        private bool OpenConnection() {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection() {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(Applicant applicant) {
+            string q = "INSERT INTO Current_JA (First_Name, Last_Name, Dob, EmailAddress, Applying_Position, Picture, Phone_Number) "
+                + $"VALUES('{applicant.First_Name}','{applicant.Last_Name}','{applicant.Dob}','{applicant.Email}',"
+                + $"'{applicant.Applying_Position}','{applicant.Picture}','{applicant.Phone_Number}')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(q, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update() {
+            string query = "UPDATE Current_JA SET name='Joe', age='22' WHERE name='John Smith'";
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Delete statement
+        public void Delete() {
+            string query = "DELETE FROM Current_JA WHERE name='John Smith'";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+    }
+}
