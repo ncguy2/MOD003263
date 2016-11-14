8c53d88dc73425d4268f2c58c280c18831d43fb8
ElementalPanda <callum.highley@live.co.uk>
2016-11-14 14:31:54 +0000

    Created new class DatabaseAccessor

--- Mod003263/Mod003263/db/DatabaseConnection.cs
+++ Mod003263/Mod003263/db/DatabaseConnection.cs
@@ -7,14 +7,14 @@ using Mod003263.interview;
 using MySql.Data.MySqlClient;
 
 namespace Mod003263.DBstuff {
-    class Database_Connection {
+    class DatabaseConnection {
         private MySqlConnection connection;
         private string server;
         private string database;
         private string uid;
         private string password;
 
-        public Database_Connection() {
+        public DatabaseConnection() {
             Initialize();
         }
 
@@ -56,7 +56,7 @@ namespace Mod003263.DBstuff {
             }
         }
 
-        private void Insert(String q) {
+        public static void Insert(String q) {
             //open connection
             if (this.OpenConnection() != true) return;
             //create command and assign the query and connection from the constructor
@@ -66,24 +66,6 @@ namespace Mod003263.DBstuff {
             //close connection
             this.CloseConnection();
         }
-
-        //Insert statement
-        public void Insert(Applicant applicant) {
-            string q = "INSERT INTO Current_JA (First_Name, Last_Name, Dob, EmailAddress, Applying_Position, Picture, Phone_Number)"
-                + $"VALUES('{applicant.First_Name}','{applicant.Last_Name}','{applicant.Dob}','{applicant.Email}',"
-                + $"'{applicant.Applying_Position}','{applicant.Picture}','{applicant.Phone_Number}')";
-            Insert(q);
-        }
-        public void Insert(Question question) {
-            string q = $"INSERT INTO Questions (Question, Category) VALUES('{question.Text()}', '{question.Cat()}')";
-            Insert(q);
-        }
-        public void Insert(int questionId, Answer answer) {
-            string q = $"INSERT INTO Question_Answers (Question_ID, Value, Weight) " +
-                       $"VALUES( '{questionId}','{answer.Text}', '{answer.Weight}')";
-            Insert(q);
-        }
-
         //Update statement
         public void Update() {
             string query = "UPDATE Current_JA SET name='Joe', age='22' WHERE name='John Smith'";
