8b5a5fc49b4e35af88f6af757a38a93511189dbb
Unknown <unknown@unknown.com>
2016-11-30 11:01:46 +0000

    Added Database file to Assets

--- Mod003263/Mod003263/db/DatabaseAccessor.cs
+++ Mod003263/Mod003263/db/DatabaseAccessor.cs
@@ -28,16 +28,16 @@ namespace Mod003263.db {
             string q = "INSERT INTO Current_JA (First_Name, Last_Name, Dob, EmailAddress, Applying_Position, Picture, Phone_Number) "
                        + $"VALUES('{applicant.First_Name}','{applicant.Last_Name}','{applicant.Dob}','{applicant.Email}',"
                        + $"'{applicant.Applying_Position}','{applicant.Picture}','{applicant.Phone_Number}')";
-            return DatabaseConnection.GetInstance().Insert(q);
+            return DatabaseConnection.GetInstance().ExecuteQuery(q);
         }
         public int Insert(Question question) {
             string q = $"INSERT INTO Questions (Question, Category) VALUES('{question.Text()}', '{question.Cat()}')";
-            return DatabaseConnection.GetInstance().Insert(q);
+            return DatabaseConnection.GetInstance().ExecuteQuery(q);
         }
         public int Insert(int questionId, Answer answer) {
             string q = $"INSERT INTO Question_Answers (Question_ID, Value, Weight) " +
                        $"VALUES( '{questionId}','{answer.Text}', '{answer.Weight}')";
-            return DatabaseConnection.GetInstance().Insert(q);
+            return DatabaseConnection.GetInstance().ExecuteQuery(q);
         }
 
         public List<InterviewFoundation> SelectAllInterviewFoundations() {
