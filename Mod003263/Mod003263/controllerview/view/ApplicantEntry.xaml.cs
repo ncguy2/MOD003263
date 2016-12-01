8eecf9e434737e0f8dd203a3269dce2c03fe7c49
ncguy2 <nick.guy@hotmail.co.uk>
2016-12-01 15:31:09 +0000

    Time conversion utils

--- Mod003263/Mod003263/controllerview/view/ApplicantEntry.xaml.cs
+++ Mod003263/Mod003263/controllerview/view/ApplicantEntry.xaml.cs
@@ -14,15 +14,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Mod003263.db;
+using Mod003263.utils;
 using Mod003263.wpf;
 
 /**
- * Author: Ryan Cowell
+ * Author: Ryan Cowell, Nick Guy
  * Date: 30/11/2016
  * Contains: ApplicantEntry
  */ 
-namespace Mod003263.controllerview.view
-{
+namespace Mod003263.controllerview.view {
     /// <summary>
     /// Interaction logic for ApplicantEntry.xaml
     /// </summary>
@@ -35,14 +35,13 @@ namespace Mod003263.controllerview.view
         }
 
         private void btn_SelectPicture_Click(object sender, RoutedEventArgs e) {
+            if (app == null) return;
 
             OpenFileDialog ofd = new OpenFileDialog();
             bool? result = ofd.ShowDialog();
             ofd.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
 
-            if (result == true) {
-                txt_FileName.Text = ofd.FileName;
-            }
+            if (result == true) txt_FileName.Text = ofd.FileName;
 
             try {
                 if (txt_FileName.Text.Trim().Length == 0) return;
@@ -53,13 +52,41 @@ namespace Mod003263.controllerview.view
                 src.EndInit();
                 ((ImageBrush)img_Applicant.Background).ImageSource = src;
                 app.Picture = Base64Converter.GetInstance().ConvertToBase64(src);
-            }
-            catch (Exception exc) {
-            }
+            } catch (Exception exc) {}
         }
 
         private void btn_newApp_Click(object sender, RoutedEventArgs e) {
             app = new Applicant{Id = -1};
+
         }
+
+        public void PopulateFields(Applicant app) {
+            SetDisabled(app == null);
+            ImageBrush b = img_Applicant.Background as ImageBrush;
+            if (b != null) b.ImageSource = Base64Converter.GetInstance().ConvertToBitmapImage(app?.Picture ?? Base64Converter.TEST_IMAGE);
+            txt_FirstName.Text = app?.First_Name ?? "";
+            txt_LastName.Text = app?.Last_Name ?? "";
+            txt_Email.Text = app?.Email ?? "";
+            txt_PhoneNumber.Text = app?.Phone_Number ?? "";
+            txt_Address.Text = app?.First_Name ?? "";
+
+            dat_DoB.Text = app != null ? ConversionUtils.ToDateTimeFromEpoch(app.Dob).ToLongDateString() : "";
+            txt_FirstName.Text = app?.First_Name ?? "";
+        }
+
+        public void SetDisabled(bool disabled) {
+            // Don't ask, I don't know either
+            bool enabled = !disabled;
+            btn_SelectPicture.IsEnabled = enabled;
+            txt_FirstName.IsEnabled = enabled;
+            txt_LastName.IsEnabled = enabled;
+            txt_Email.IsEnabled = enabled;
+            sel_ApplyingPosition.IsEnabled = enabled;
+            txt_PhoneNumber.IsEnabled = enabled;
+            txt_Address.IsEnabled = enabled;
+            dat_DoB.IsEnabled = enabled;
+            btn_Save.IsEnabled = enabled;
+        }
+
     }
 }
---
+++ Mod003263/Mod003263/utils/ConversionUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/**
+ *  Author: Nick Guy, http://stackoverflow.com/a/29908680
+ *  Date: 01/12/2016
+ *  Contains: Conversion Utils
+ */
+namespace Mod003263.utils {
+    public class ConversionUtils {
+
+        public static long ToEpochTime(DateTime dateTime) {
+            DateTime date = dateTime.ToUniversalTime();
+            long ticks = date.Ticks - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).Ticks;
+            long ts = ticks / TimeSpan.TicksPerSecond;
+            return ts;
+        }
+
+        public static long ToEpochTime(DateTimeOffset dateTime) {
+            DateTimeOffset date = dateTime.ToUniversalTime();
+            long ticks = date.Ticks - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero).Ticks;
+            long ts = ticks / TimeSpan.TicksPerSecond;
+            return ts;
+        }
+
+        public static DateTime ToDateTimeFromEpoch(long intDate) {
+            long timeInTicks = intDate * TimeSpan.TicksPerSecond;
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddTicks(timeInTicks);
+        }
+
+        public static DateTimeOffset ToDateTimeOffsetFromEpoch(long intDate) {
+            long timeInTicks = intDate * TimeSpan.TicksPerSecond;
+            return new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero).AddTicks(timeInTicks);
+        }
+
+    }
+}
